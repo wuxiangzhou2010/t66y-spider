06175a19e396c91389690e13a3ff6108e60e18f4
xihajuan2010 <xihajuan2010@gmail.com>
2017-07-22 22:23:38 +0800

    1. add GuoChanYuanChuang support 2. PEP8

--- t66ySpider/t66ySpider/items.py
+++ t66ySpider/t66ySpider/items.py
@@ -4,27 +4,38 @@ import scrapy
 
 
 class T66YspiderDagaierItem(scrapy.Item):
-    t_title         = scrapy.Field()
-    t_image_list    = scrapy.Field()
+    t_title = scrapy.Field()
+    t_image_list = scrapy.Field()
+
 
 class T66YspiderXinshidaiItem(scrapy.Item):
-    t_title         = scrapy.Field()
-    t_image_list    = scrapy.Field()
+    t_title = scrapy.Field()
+    t_image_list = scrapy.Field()
+
 
 class T66YspiderYazhouwumaItem(scrapy.Item):
-    t_title         = scrapy.Field()
-    t_url           = scrapy.Field()
-    t_image_list    = scrapy.Field()
-    t_torrent_list  = scrapy.Field()
+    t_title = scrapy.Field()
+    t_url = scrapy.Field()
+    t_image_list = scrapy.Field()
+    t_torrent_list = scrapy.Field()
+
 
 class T66YspiderYazhouyoumaItem(scrapy.Item):
-    t_title         = scrapy.Field()
-    t_url           = scrapy.Field()
-    t_image_list    = scrapy.Field()
-    t_torrent_list  = scrapy.Field()
+    t_title = scrapy.Field()
+    t_url = scrapy.Field()
+    t_image_list = scrapy.Field()
+    t_torrent_list = scrapy.Field()
+
+
+class T66YspiderGuochanyuanchuangItem(scrapy.Item):
+    t_title = scrapy.Field()
+    t_url = scrapy.Field()
+    t_image_list = scrapy.Field()
+    t_torrent_list = scrapy.Field()
+
 
 class T66YspiderDongmanItem(scrapy.Item):
-    t_title         = scrapy.Field()
-    t_url           = scrapy.Field()
-    t_image_list    = scrapy.Field()
-    t_torrent_list  = scrapy.Field()
+    t_title = scrapy.Field()
+    t_url = scrapy.Field()
+    t_image_list = scrapy.Field()
+    t_torrent_list = scrapy.Field()
